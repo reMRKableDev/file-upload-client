0ceb7bb5aa49ce528f5f5377535a7db5ad8d510e
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-09-15 11:06:44 +0200

    :art: New commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AddThing from "./AddThing";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>File Upload</h1>
+      <AddThing />
+    </div>
+  );
+}
+
+export default App;
